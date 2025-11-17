8292c944c45f7f0a00cad7446604aba96adc757c
𝒮𝑒𝓋𝒾𝓃𝒸𝒽 <ysevinchoy2197@gmail.com>
2025-11-17 16:01:54 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,55 @@
+#1-misol
+s = "abracadabra"
+print(s)
+
+toza = s[4:8]
+print(toza)
+
+#2-misol
+s = "absdefghijk"
+print(s)
+
+toza = s[3:3]
+print(toza)
+
+#3-misol
+s = "GameOverMission"
+print(s)
+
+toza = s[4:8]
+print(slice)
+
+#4-misol
+s = "123456789"
+print(s)
+
+toza = s[-5]
+print(toza)
+
+#5-misol
+s = "indexing"
+print(s)
+
+toza = s[2:5]
+print(toza)
+
+#6-misol
+s = "slicingIsPowerful"
+print(s)
+
+toza = s[4:]
+print(toza)
+
+#7-misol
+s = "absdefghij"
+print(s)
+
+toza = s[-5: -2]
+print(toza)
+
+#8-misol
+s = "ABSDEFGHIJ"
+print(s)
+
+toza = s[-2:-8:-2]
+print(toza)
